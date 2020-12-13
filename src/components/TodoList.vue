1724202773d6ca263ff3b931691d70dbea2052e9
azahara <azaharamaga@gmail.com>
2020-12-13 11:23:09 +0100

    Create form to add taks

---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="createTask">
+    <label class="label" for="task">New task:</label>
+    <input type="text" id="task" v-model="newTask" />
+    {{ newTask }}
+    <input type="submit" value="Create task" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data: () => ({
+    newTask: "",
+    tasks: []
+  }),
+  methods: {
+    createTask(){
+      let task = {
+        text: this.newTask,
+        completed: false
+      }
+      this.tasks.push(task)
+      this.newTask = ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
